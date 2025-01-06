d50e6d7c7b1a63d2ca97d06543e1c33ef49e5973
Ashim Gautam <computer77356@gmail.com>
2025-01-06 16:23:50 +0545

    added route for signup and signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello ji kyaa hallchall</h1>
+    <div className="text-lg w-screen h-screen flex flex-col justify-center items-center">
+      <div>
+
+        <p>hello ji kyaa hallchall</p>
+      </div>
+      <div className=" mt-10 w-[50%] flex justify-evenly">
+        <Link href={"/signin"} className="border rounded-md p-4">Signin to todo app</Link>
+        <Link href={"signup"} className="border rounded-md p-4">Signup to todo app</Link>
+      </div>
     </div>
   );
 }
