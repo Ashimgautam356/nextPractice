308278ce9a32f7fa90774f6cd4a73cdf021e06aa
Ashim Gautam <computer77356@gmail.com>
2025-01-06 20:43:39 +0545

    added primsa to the server

--- app/api/v1/signup/route.ts
+++ app/api/v1/signup/route.ts
@@ -1,8 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+
+const client = new PrismaClient();
+
 
 export async function POST(req:NextRequest){
 
     const data = await  req.json()
+    client.user.create({
+        data:{
+            userName:data.username,
+            password:data.password
+        }
+    })
+
     return NextResponse.json({
         message:"you have been signup"
     })
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,12 +1,15 @@
 "use client"
+
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { ChangeEventHandler, useState } from "react";
 
 export default function Signup() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter();
 
-    return <div className="h-screen flex justify-center flex-col">
+    return (<div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                 <div>
@@ -23,16 +26,18 @@ export default function Signup() {
                             setPassword(e.target.value)
                         }} label="Password" type={"password"} placeholder="123456" />
                         <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                        onClick={()=>{ axios.post("http://localhost:3000/api/v1/signup",{
+                        onClick={async()=>{ axios.post("http://localhost:3000/api/v1/signup",{
                             username,
                             password
-                        })}}
+                        })
+                        router.push("/signin")
+                    }}
                         >Sign up</button>
                     </div>
                 </div>
             </a>
         </div>
-    </div>
+    </div>)
 
 }
 
